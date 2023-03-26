6f8f97ae42e05bc548204d626a242462fe48ac99
hotic06 <40561364+hotic06@users.noreply.github.com>
2023-03-26 23:18:53 +0900

    Update build-docker.sh
    PROXY設定を読み込むようにした。

--- build-docker.sh
+++ build-docker.sh
@@ -1,2 +1,7 @@
 cd docker
-docker build ./ -t airobotbook/ros2-desktop-ai-robot-book-nvidia
+docker build ./ \
+--build-arg http_proxy=${http_proxy} \
+--build-arg https_proxy=${https_proxy} \
+--build-arg HTTP_PROXY=${HTTP_PROXY} \
+--build-arg HTTPS_PROXY=${HTTPS_PROXY} \
+-t airobotbook/ros2-desktop-ai-robot-book-nvidia
