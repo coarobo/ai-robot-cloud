d3c8c103622ec1df47e56dc50b4eb69e098a57b8
Ubuntu <ubuntu@ip-172-31-34-111.ap-northeast-1.compute.internal>
2023-03-27 03:58:58 +0000

    update relating xrandr

--- docker/rootfs/startup_nvidia.sh
+++ docker/rootfs/startup_nvidia.sh
@@ -3,6 +3,10 @@
 # VNC解像度設定
 if [ -n "$RESOLUTION" ]; then
     nvidia-xconfig --no-use-edid-dpi -a --virtual=$RESOLUTION --allow-empty-initial-configuration --enable-all-gpus --busid `nvidia-xconfig --query-gpu-info | grep BusID | awk '{print substr($0, index($0, " : ")+3)}'`
+    echo "#!/bin/sh" > /usr/local/bin/xrandr_exec.sh
+    echo "xrandr --fb $RESOLUTION" >> /usr/local/bin/xrandr_exec.sh
+    echo "xrandr -s $RESOLUTION" >> /usr/local/bin/xrandr_exec.sh
+    chmod +x /usr/local/bin/xrandr_exec.sh
 fi
 
 exec /startup.sh
