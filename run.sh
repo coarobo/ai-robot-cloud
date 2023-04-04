d25368defd6942fa9f1fdc889a5d95a2a35691b8
Ubuntu <ubuntu@ip-172-31-34-111.ap-northeast-1.compute.internal>
2023-04-04 09:44:58 +0000

    mount user directory

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+HOST_WS_DIR=/home/ubuntu/catkin_ws/src/ros2-desktop-nvidia/user
+CONTAINER_WS_DIR=/home/ubuntu/Desktop/user
+
 docker run -d \
 -e http_proxy=${http_proxy} \
 -e https_proxy=${https_proxy} \
@@ -14,5 +17,6 @@ docker run -d \
 -p 6080:80 \
 --shm-size=512m \
 --privileged \
+--mount type=bind,src=${HOST_WS_DIR},dst=${CONTAINER_WS_DIR} \
 $* \
 airobotbook/ros2-desktop-ai-robot-book-nvidia:latest
